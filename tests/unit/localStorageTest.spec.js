025c7b12094b767a90b93b8bcf640993c8749eba
Tae Kim <taekim.dev@gmail.com>
2024-07-05 07:41:55 +0800

    add a local storage test

---
+++ tests/unit/localStorageTest.spec.js
@@ -0,0 +1,12 @@
+describe('Local Storag Test', () => {
+    it('shold store and retrieve items from local storage', () => {
+        const storageKey = 'storage-key';
+        const storageValue = 'storage-value';
+
+        localStorage.setItem(storageKey, storageValue);
+        const retrievedValue = localStorage.getItem(storageKey);
+
+        expect(retrievedValue).toBe(storageValue);
+    })
+
+});
