91b9ba7ddc2b1ded4d35019287ec3070959edd53
Tae Kim <taekim.dev@gmail.com>
2024-08-04 08:00:04 +0800

    enable sourcemap in local

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "development") {
+      config.devtool = "source-map";
+    }
+  },
 });
